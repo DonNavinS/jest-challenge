92794f23286572d68f467d0c1c0076aa41d54299
DonNavinS <seneviratnanavin@gmail.com>
2021-06-25 10:17:58 -0400

    Added CSS to generated HTML

--- HTMLContent.js
+++ HTMLContent.js
@@ -1,5 +1,22 @@
 const HTMLContent = function(answers){
-    return `<h1>${answers.Title}</h1>
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Roster</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+    <h1>${answers.Title}</h1>
+        
+    </body>
+    </html>
+  
+    
+    
+   
     
     
     
